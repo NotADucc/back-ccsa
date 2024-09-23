5144b69622998d022210b7a1f7aacdf697910d15
Guillaume Desktop <robertohelper@outlook.com>
2024-09-23 23:14:06 +0200

    feature: gcd is now with rec

--- 04B-grootste-gemene-deler.py
+++ 04B-grootste-gemene-deler.py
@@ -1,5 +1,14 @@
+# Iter
 def ggd(g1, g2) :
     (a, b) = (g1, g2) if g1 > g2 else (g2, g1)
     while b != 0 :
         (a, b) = (b, a % b)
     return a
+    
+# Rec
+def ggd(g1, g2) :
+    (a, b) = (g1, g2) if g1 > g2 else (g2, g1)
+    return rec(a, b)
+
+def rec(a, b) :
+    return a if not b else ggd(b, a % b)
